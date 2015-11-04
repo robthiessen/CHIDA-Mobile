e2186b0d9c02d3bfd2107bdacb86bd059c45fd74
shutter111 <rob@richmondday.com>
2015-11-04 08:10:48 -0500

    mobile updates

--- js/CMHA.js
+++ js/CMHA.js
@@ -13,6 +13,7 @@ var App = CMHA.App = (function () {
 	  function App($location){
 		
 		 this.location = $location;
+		 this.data = [];
 		 this.init();
 	  }
 	
@@ -30,13 +31,11 @@ var App = CMHA.App = (function () {
 		  DESCRIBE : 
 	      */
 		  optionEvents : function () {
-			  
-			 /* if (window.navigator.standalone === false) {
-				  alert("this App can be saved to your home screen.");
-			  }
-			  */
+			   
 			  
 			 var scope = this;
+
+			 this.data = new CMHA.Data();
 			 
 			 this.fadeInIntroText();
 			 $('.branding').fadeIn();
@@ -46,8 +45,10 @@ var App = CMHA.App = (function () {
 				 var li = $(this).parent();
 				 var target = $(this).parent().attr('id');
 				 var open = false; 
+				 var id = $(this).parent().attr('id');
+				 scope.loadData(id,scope.data.getData(id));
 				 
-				 scope.addData(t,target);
+				  
 				 $('.data-block').slideUp(299);
 			     $('.section li').css({'border-width':'2px'});
 				 
@@ -95,14 +96,43 @@ var App = CMHA.App = (function () {
 		  /**
 		  Description : 
 		  */
-		  addData : function (div,target) {
+		  loadData : function (id,data) {
 			  
-			console.log(div + " ::::::: " + target );  
+			//console.log(div + " ::::::: " + target );  
 			
-			
+			for(var i = 0; i < data.length; i++ ){
+				var item = this.template(data[i]);
+                $("#"+id).find('.data-block').append(item);
+			}
 			
 			 // '<div class="item" ng-repeat="item in Abuse" ng-include="'partials/ServiceTemplate.html'"  repeat-done="layoutReady()"> </div>'; 
 			  
+		  },
+
+		  template : function (item) {
+             
+              var tmp = '<h3>'+item.title+'</h3>'+
+						'<p ng-if="item.ShortText">'+item.ShortText+'</p>'+
+						'<div class="phone-number" ng-if="item.phone">'+
+						'<div class="action-icon icon-call"><a href="tel:'+item.phone+'"><h5 class="tele">'+item.phone+'</h5></a></div>'+
+						'</div>'+
+						'<div class="phone-number">'+
+						'<div class="action-icon icon-call"><a href="tel:'+item.tollFree+'"><h5 class="tele">'+item.tollFree+'</h5></a></div>'+
+						'</div>'+
+						'<div class="phone-number">'+
+						'<div class="action-icon icon-call"><a href="tel:'+item.mobile+'"><h5 class="tele">'+item.mobile+'</h5></a></div>'+
+						'</div>'+
+
+						'<div class="phone-number">'+
+						'<div class="action-icon icon-mail"><a href="mailto:'+item.email+'"><h5 class="tele">'+item.email+'</h5></a></div>'+
+						'</div>'+
+
+						'<div class="phone-number">'+
+						'<div class="action-icon icon-site"><a href="http://'+item.website+'"><h5 class="tele">'+item.website+'</h5></a></div>'+
+						'</div>';
+               
+               return tmp;
+
 		  },
 		  
 		  
